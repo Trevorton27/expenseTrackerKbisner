03972fb6471b2f44bb942a25e5d6f9cc621881a8
Trevor Mearns <Trevor Mearns>
2020-11-28 17:39:31 +0900

    issues with delete functionality

--- expense.js
+++ expense.js
@@ -1,166 +1,125 @@
-let expenseArray = [];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-const dataButton = document.getElementById("data-button");
-const inputDate = document.getElementById("date");
-const inputType = document.getElementById("type");
-const inputMerchant = document.getElementById("merchant");
-const inputAmount = document.getElementById("amount");
-const expenseTable = document.getElementById("expense-table");
-const expenseInputContainer = document.getElementById("expense-input-container");
-const elementTableToClear = document.getElementById("expense-table");
+const expenseArray = JSON.parse(localStorage.getItem('allExpenses')) || [];
 
-dataButton.addEventListener("click", validateData);
+const dataButton = document.getElementById('data-button');
+const inputDate = document.getElementById('date');
+const inputType = document.getElementById('type');
+const inputMerchant = document.getElementById('merchant');
+const inputAmount = document.getElementById('amount');
+const expenseTable = document.getElementById('expense-table');
+const expenseInputContainer = document.getElementById(
+  'expense-input-container'
+);
+const elementTableToClear = document.getElementById('expense-table');
+const form = document.getElementById('form');
 
-function trackExpenses() {
-  pushDataToArray();
-  saveRawData();
-  clearTableDisplay();
-  getRawData();
+window.addEventListener('load', updateTable);
+
+function updateTable() {
+  expenseArray.forEach((savedExpense) => {
+    displayData(savedExpense);
+  });
 }
 
+dataButton.addEventListener('click', validateData);
+
+function pushExpense() {
+  const newExpenseItem = {
+    id: Math.random(),
+    date: inputDate.value,
+    paymentType: inputType.value,
+    merchant: inputMerchant.value,
+    amount: inputAmount.value
+  };
+
+  pushDataToArray(newExpenseItem);
+  saveRawData(newExpenseItem);
+  //clearTableDisplay();
+  //getRawData();
+  displayData(newExpenseItem);
+  form.reset();
+}
+
+function pushDataToArray(newExpenseItem) {
+  expenseArray.push(newExpenseItem);
+}
 function validateData() {
-  const dataErrorMessage = document.createElement("p");
-  
-  if (document.getElementById("data-error-message")) {
-    document.getElementById("data-error-message").remove();
+  const dataErrorMessage = document.createElement('p');
+
+  if (document.getElementById('data-error-message')) {
+    document.getElementById('data-error-message').remove();
   }
 
-  if (inputDate.value === "" || inputMerchant.value === "" || inputAmount.value === "") {
-    dataErrorMessage.setAttribute("id", "data-error-message");
-    dataErrorMessage.appendChild(document.createTextNode("You must enter all fields to continue!"));
+  if (
+    inputDate.value === '' ||
+    inputMerchant.value === '' ||
+    inputAmount.value === ''
+  ) {
+    dataErrorMessage.setAttribute('id', 'data-error-message');
+    dataErrorMessage.appendChild(
+      document.createTextNode('You must enter all fields to continue!')
+    );
     expenseInputContainer.appendChild(dataErrorMessage);
-    return
+    return;
   }
-  trackExpenses();
-}
-
-
-function pushDataToArray() {
-  dateOfExpense = toDate(inputDate.value);
-  expenseArray.push([dateOfExpense, inputType.value, inputMerchant.value, "$ " + Number(inputAmount.value).toFixed(2)]);
+  pushExpense();
 }
 
 function saveRawData() {
-  localStorage.clear();
-  expenseArray = expenseArray.sort().reverse();
-  localStorage.setItem("allExpenses", JSON.stringify(expenseArray));
-  inputDate.value = "";
-  inputType.value = "";
-  inputMerchant.value = "";
-  inputAmount.value = "";
+  localStorage.setItem('allExpenses', JSON.stringify(expenseArray));
 }
 
-function getRawData() {
-  const allItemsInStorage = JSON.parse(localStorage.getItem("allExpenses")) || [];
+function displayData(newExpenseItem) {
+  const rowId = newExpenseItem.id;
+  const newTableRow = document.createElement('tr');
+  newTableRow.setAttribute('id', rowId);
 
-  expenseArray = [];
-  allItemsInStorage.forEach(item => {
-    expenseArray.push(item);
-  });
+  const tableRowDate = document.createElement('td');
+  tableRowDate.setAttribute('id', 'expenseItemDate');
 
-  if (expenseArray.length > 0) {
-    clearTableDisplay();
+  const tableRowPaymentType = document.createElement('td');
+  tableRowPaymentType.setAttribute('id', 'expenseItemType');
 
-    expenseArray.forEach(arr => {
-      displayData(arr)
-    });
-  }
-}
+  const tableRowMerchant = document.createElement('td');
+  tableRowMerchant.setAttribute('id', 'expenseItemMerchant');
 
-function displayData(arrayToDisplay) {
-  const itemDate = arrayToDisplay[0];
-  const itemType = arrayToDisplay[1];
-  const itemMerchant = arrayToDisplay[2];
-  const itemAmount = arrayToDisplay[3];
-  const newTableRow = document.createElement("tr");
-  const newTableDataDate = document.createElement("td");
-  const newTableDataType = document.createElement("td");
-  const newTableDataMerchant = document.createElement("td");
-  const newTableDataAmount = document.createElement("td");
-  const newTableDataDelete = document.createElement("td");
-  const newTableDataDeleteImg = document.createElement("img");
+  const tableRowAmount = document.createElement('td');
+  tableRowAmount.setAttribute('id', 'expenseItemAmount');
 
-  newTableRow.setAttribute("id", "expenseRow");
+  const tableRowDelete = document.createElement('td');
 
-  newTableDataDate.setAttribute("id", "expenseItemDate");
-  newTableDataDate.appendChild(document.createTextNode(itemDate));
-
-  newTableDataType.setAttribute("id", "expenseItemType");
-  newTableDataType.appendChild(document.createTextNode(itemType));
-
-  newTableDataMerchant.setAttribute("id", "expenseItemMerchant");
-  newTableDataMerchant.appendChild(document.createTextNode(itemMerchant));
-  
-  newTableDataAmount.setAttribute("id", "expenseItemAmount");
-  newTableDataAmount.appendChild(document.createTextNode(itemAmount));
-
-  newTableDataDelete.setAttribute("id", "task-delete");
-  
-  newTableDataDeleteImg.setAttribute("class", "task-delete-img");
-  newTableDataDeleteImg.setAttribute("src", "images/redx.png");
-  newTableDataDeleteImg.setAttribute("alt", "red brushstroke x");
-  
-  newTableDataDelete.appendChild(newTableDataDeleteImg);
-  newTableRow.appendChild(newTableDataDate);
-  newTableRow.appendChild(newTableDataType);
-  newTableRow.appendChild(newTableDataMerchant);
-  newTableRow.appendChild(newTableDataAmount);
-  newTableRow.appendChild(newTableDataDelete);
+  const newTableDataDeleteImg = document.createElement('img');
+  newTableDataDeleteImg.setAttribute('id', 'task-delete-img');
+  newTableDataDeleteImg.setAttribute('src', 'images/redx.png');
+  newTableDataDeleteImg.setAttribute('alt', 'red brushstroke x');
+  tableRowDelete.appendChild(newTableDataDeleteImg);
+  tableRowDelete.setAttribute('id', 'task-delete');
 
   expenseTable.appendChild(newTableRow);
 
-  createEventListener();
-}
+  newTableRow.appendChild(tableRowDate);
+  newTableRow.appendChild(tableRowPaymentType);
+  newTableRow.appendChild(tableRowMerchant);
+  newTableRow.appendChild(tableRowAmount);
+  newTableRow.appendChild(tableRowDelete);
 
-function clearTableDisplay() {
-  while (elementTableToClear.firstChild) {
-    elementTableToClear.removeChild(elementTableToClear.firstChild);
-  }
-}
+  tableRowDate.textContent = newExpenseItem.date;
+  tableRowPaymentType.textContent = newExpenseItem.paymentType;
+  tableRowMerchant.textContent = newExpenseItem.merchant;
+  tableRowAmount.textContent = newExpenseItem.amount;
 
-function toDate(dateString) {
-  const dateToSplit = dateString.split("-");
-  const convertedDate = dateToSplit[1] + "-" + dateToSplit[2] + "-" + dateToSplit[0];
-  return convertedDate
-}
-
-function createEventListener() {
-  const deleteButton = document.getElementsByClassName("task-delete-img");
-
-  if (deleteButton) {
-    Array.from(deleteButton).forEach(element => {
-      element.addEventListener("click", deleteExpense);
-    });
-  }
+  const deleteButton = document.getElementById('task-delete');
+  deleteButton.addEventListener('click', deleteExpense);
 }
 
 function deleteExpense(e) {
-  const nodeNthChildToRemove = e.target.parentNode.parentNode.parentNode;
-  const nthChildToRemove = countSiblings(e.target);
-
-  nodeNthChildToRemove.removeChild(nodeNthChildToRemove.childNodes[nthChildToRemove]);
-  expenseArray.splice(nthChildToRemove, 1);
-
-  saveRawData();
-}
-
-function countSiblings(target) {
-
-  let currentNode = target;
-  let parentNodeExpenseRow = target.parentNode.parentNode;
-  let previousSiblingNode = parentNodeExpenseRow.previousSibling;
-  let numberOfSiblings = 0;
-
-  while (previousSiblingNode !== null) {
-    numberOfSiblings += 1;
-    currentNode = previousSiblingNode;
-    previousSiblingNode = previousSiblingNode.previousSibling;
+  e.preventDefault();
+  console.log(e.target.parentNode.id);
+  let rowId = e.target.parentNode.id;
+  for (let i = 0; i < expenseArray.length; i++) {
+    if (expenseArray[i].id === rowId) {
+      expenseArray.splice(i, 1);
+      rowId.parentNode.removeChild(rowId);
+      saveRawData();
+    }
   }
-  return numberOfSiblings
 }
-
-getRawData();
-inputDate.value = "";
-inputType.value = "";
-inputMerchant.value = "";
-inputAmount.value = "";
